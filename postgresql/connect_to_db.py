60cdf3bf92a41a3e90ede0054fc4598d5c8da0c4
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2022-12-09 22:35:29 +0200

    Update connect_to_db.py

--- postgresql/connect_to_db.py
+++ postgresql/connect_to_db.py
@@ -1,12 +1,12 @@
 import mysql.connector 
 
 myconnection = mysql.connector.connect(
-    host = "localhost" ,
-    user = "root",
-    passwd = "toor"
+    host = "localhost" ,                  # local host name
+    user = "root",                        # mysql username
+    passwd = "toor"                       # mysql password
 
 )
 
-mycursor = myconnection.cursor()
-mycursor.execute("")
+mycursor = myconnection.cursor()          # my cursor to perform database operations
+mycursor.execute("")                      # perform queries on database
 
